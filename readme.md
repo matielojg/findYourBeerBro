697d495e484002686f5664c16fc28d43998073ab
Matielo Geronimo <matielo.dev@gmail.com>
2022-12-23 01:01:36 -0300

    added node --watch instead of nodemon

--- readme.md
+++ readme.md
@@ -1,1 +1,2 @@
 npm init -y
+node --watch server.js
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const http = require("http")
 const port = 3000;
 
+const rotas = {
+    '/': 'Posto de bebidas',
+    '/distribuidoras': 'Entrei na pag de distribuidoras',
+    '/marcas': 'Listagem de marcas de cervejas',
+    '/sobre': "Info sobre o sistema"
+
+}
 const server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/plain' })
-    res.end('Curso de Node');
+    res.end(rotas[req.url]);
 })
 server.listen(port, () => {
     console.log(`Servidor escutando em http://localhost:${port}`)
