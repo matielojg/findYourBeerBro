6156ee4946c88954d6035b2432825012f635b2db
Matielo Geronimo <matielo.dev@gmail.com>
2022-12-27 02:42:10 -0300

    added method /post

--- src/controllers/marcasController.js
+++ src/controllers/marcasController.js
@@ -5,7 +5,16 @@ class MarcaController {
         marcas.find((err, marcas) => {
             res.status(200).json(marcas)
         })
-
+    }
+    static cadastrarMarca = (req, res) => {
+        let marca = new marcas(req.body);
+        marca.save((err) => {
+            if (err) {
+                res.status(500).send({ message: `${err.message} - falha ao cadastrar marca.` })
+            } else {
+                res.status(201).send(marca.toJSON());
+            }
+        });
     }
 }
 export default MarcaController
